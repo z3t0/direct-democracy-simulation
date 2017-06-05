55e47efaae910cf08938133134ee8ccaca68f8fd
Rafi Khan <zetoslab@gmail.com>
2017-06-05 13:37:20 -0600

    primitive voting system

--- poll/urls.py
+++ poll/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'poll'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # ex: /poll/
+    url(r'^$', views.index, name='index'),
+    # ex: /poll/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /poll/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /poll/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
