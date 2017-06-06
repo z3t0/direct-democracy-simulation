ee368b18a505e9c8dda5b7a0ec76401edab46df7
Rafi Khan <zetoslab@gmail.com>
2017-06-06 01:09:28 -0600

    citizen related to user

--- forum/models.py
+++ forum/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Citizen(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
     votes = models.IntegerField(default=0)
     name = models.CharField(max_length=20)
+
+    @receiver(post_save, sender=User)
+    def create_user_citizen(sender, instance, created, **kwargs):
+        if created:
+            Citizen.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_citizen(sender, instance, **kwargs):
+        instance.citizen.save()
 
 
 class Issue(models.Model):
