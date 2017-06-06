6101d3d48cc555055d68744b6b7ade654ff73437
Rafi Khan <zetoslab@gmail.com>
2017-06-06 16:04:41 -0600

    added description to issue form

--- forum/forms.py
+++ forum/forms.py
@@ -3,4 +3,5 @@ from django import forms
 
 class IssueForm(forms.Form):
     issue_title = forms.CharField(label='Issue title', max_length=50)
+    issue_description = forms.CharField(label='Description', max_length=1000)
     print("New issue request received")
