6e223e7c4f1edfb187c4c30ce9c1a1af2374f710
Rafi Khan <zetoslab@gmail.com>
2017-06-06 16:32:07 -0600

    adding issues

--- forum/views.py
+++ forum/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-import logging
 
 from .models import Issue
 from .forms import IssueForm
-
-logger = logging.getLogger()
 
 
 @login_required
@@ -14,10 +11,15 @@ def index(request):
     if request.method == 'POST':
         form = IssueForm(request.POST)
 
+
         if form.is_valid():
-            # process data
+            issue_title = form.cleaned_data["issue_title"]
+            issue_description = form.cleaned_data["issue_description"]
+            issue = Issue(title=issue_title, description=issue_description, author=request.user.citizen)
+
+            issue.save()
             # redirect
-            logger.info('processed new issue form')
+            return HttpResponseRedirect('/')
 
     else:
         form = IssueForm()
